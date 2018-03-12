2caa96919272a87a989145d3301144c026af78e9
PP <2503361622@qq.com>
2018-03-12 18:21:52 +0800

    option => input data

--- resources/views/html/wishWall/wall_2.blade.php
+++ resources/views/html/wishWall/wall_2.blade.php
@@ -13,9 +13,10 @@
             </div>
             <div class="modal-body">
                 <p>选择用户(下拉框选择)</p>
-                <select class="form-control"  id="user_select"  name="" style="width:40%" required>
+                <select class="form-control" id="user_select" name="" style="width:40%" required>
                     {{--增加用户选择--}}
                 </select>
+
                 <p>留言内容</p>
                 <textarea class="form-control" name="inputComment" required></textarea>
             </div>
@@ -30,27 +31,50 @@
 
     {{--js引用的位置会对函数产生影响--}}
     <script>
+        //        $(document).ready(function () {
+        //            //点击隐藏p元素. hide show  toggle作为隐藏显示切换
+        //
+        //            $("#bth_2").click(function(){
+        //                $("p").hide();
+        //            });
+        //        });
+
         $(function () {
-
+            //select option 选择框加载
+            //设置文本text, 设置表单val
+            //option中设置属性，
             $.ajax({
-                type:"get",
-                url:"/userAll",
-                data:{},
-                dataType:"json",
-                success:function(result){
-
-
-
-                    console.log(result);
+                type: "get",
+                url: "/userAll",
+                data: {},
+                dataType: "json",
+                success: function (result) {
+                    if (result.status == 200) {
+                        for (var i = 0; i < result.data.length; i++) {
+                            var opetion1 = document.createElement('option');
+                            opetion1.text = result.data[i].name;
+                            opetion1.setAttribute("id", result.data[i].id);
+                            opetion1.setAttribute("value", result.data[i].name);
+                            $("select#user_select").append(opetion1);
+                        }
+                    }
                 }
             });
-
-
-
-            $("#btn_2").click(function(){
-                console.log('1111');
-            });
         });
+
+
+
+        $("#bth_2").click(function () {
+            //上传数据
+            //点击发表增加加载select数据等等上传？
+//            var id = $("select#user_select").val();
+
+//            console.log($("select#user_select").val());
+        });
+
+
     </script>
+
+
 
 @endsection
